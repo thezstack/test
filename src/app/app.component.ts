d521660e1a8698fcc8260e8348d2d688e581b2ef
Zubair Mughal <zubair.mughal@avanade.com>
2018-06-09 01:13:05 -0500

    second test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+interface UserResponse {
+  login:string,
+  bio:string,
+  avatar_url:string
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private http: HttpClient){
+
+  }
+
+  ngOnInit(): void {
+    this.http.get<UserResponse>('https://api.github.com/users/thezstack').subscribe(data => {
+      console.log(data.login);
+      console.log(data.bio);
+      console.log(data.avatar_url);
+    })
+  }
 }
